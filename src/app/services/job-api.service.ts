632c4b9b301dfa0da73c11c3956829cbf83031e1
Roman <romkaras2@gmail.com>
2026-07-28 13:14:47 +0200

    Update job-api.service.ts for load error

--- src/app/services/job-api.service.ts
+++ src/app/services/job-api.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom} from 'rxjs';
 import { Job } from '../models/job.model'; 
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment.dev';
  
 @Injectable({ 
   providedIn: 'root' 
